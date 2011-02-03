0e3ac3782b77c9bd7238e92e982a7d1afa94f536
Jaime Perez <jaimeperez@trla.org>
2011-02-02 18:23:55 -0600

    i hate ie.

---
+++ offices.js
@@ -0,0 +1,96 @@
+var offices = [
+		{
+			"subject":"Alpine",
+			"status":"CLOSED Weather"
+		},
+		
+		{
+			"subject":"Austin",
+			"status":"Open"
+		},
+		{
+		
+			"subject":"Bee County PD",
+			"status":"Open"
+		},
+		{
+			"subject":"Brownsville",
+			"status":"Open"
+		},
+		{
+			"subject":"Corpus Christi Courthouse",
+			"status":"Open"
+		},
+		{
+			"subject":"Corpus Christi Pueblo",
+			"status":"Open"
+		},
+		{
+			"subject":"Del Rio",
+			"status":"Open"
+		},
+		{
+			"subject":"Eagle Pass",
+			"status":"Open"
+		},
+		{
+			"subject":"Edinburg",
+			"status":"Open"
+		},
+		{
+			"subject":"El Paso",
+			"status":"CLOSED Weather"
+		},
+		{
+			"subject":"Harlingen",
+			"status":"Open"
+		},
+		{
+			"subject":"Kerrville",
+			"status":"Open"
+		},
+		{
+			"subject":"Laredo",
+			"status":"Open"
+		},
+		{
+			"subject":"Nashville (SMLS)",
+			"status":"Open"
+		},
+		{
+			"subject":"San Antonio Main",
+			"status":"Open"
+		},
+		{
+			"subject":"Sinton",
+			"status":"Open"
+		},
+		{
+			"subject":"Victoria",
+			"status":"Open"
+		},
+		{
+			"subject":"Weslaco",
+			"status":"Open"
+		},
+		{
+			"subject":"Willacy County PD",
+			"status":"Open"
+		},
+		{
+			"subject":"<b>Citrix / CTS</b>",
+			"status":"Open"
+		},
+		{
+			"subject":"<b>MPLS Internet</b>",
+			"status":"Open"
+		},
+		{
+			"subject":"<b>Email</b>",
+			"status":"Open"
+		},
+		{
+			"subject":"<b>VoIP Phones</b>",
+			"status":"Open"
+		}
+	];
